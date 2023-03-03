7244d42768f61c000319094b27df9859173191e2
Vaishnavi Mudaliar <vaishnavim1311@gmail.com>
2023-03-03 17:38:39 +0530

    Time: 42 ms (24.20%), Space: 13.9 MB (56.32%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        c = Counter(s)
+        output = 0
+        odd_found = False
+        
+        for count in c.values():
+            if odd_found:
+                if count > 1:
+                    if count % 2 == 0:
+                        output += count
+                    else:
+                        output += count - 1
+                        
+            else:
+                if count % 2 == 0:
+                    output += count
+                else:
+                    output += count
+                    odd_found = True
+                    
+        return output
+                    
+                    
+                    
+            
+        
